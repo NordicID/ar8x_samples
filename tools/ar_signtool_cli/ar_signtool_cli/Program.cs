2353ad33e5ef84c96dd8e932344539536246c00d
Mikko Lähteenmäki <mikko.lahteenmaki@nordicid.com>
2020-12-29 12:25:08 +0200

    v1.0.0.2
    - Add support for output filename to substitute variables from package manifest
    - Check input file Directory attribute with HasFlag instead of equals

--- tools/ar_signtool_cli/ar_signtool_cli/Program.cs
+++ tools/ar_signtool_cli/ar_signtool_cli/Program.cs
@@ -10,36 +10,60 @@ namespace ar_signtool_cli
 {
     class Program
     {
+        static int PrintUsage()
+        {
+            Console.WriteLine("USAGE: ar_signtool_cli 'input file or folder' [output zip]");
+            Console.WriteLine("  output file may contain values from package manifest, e.g. ar8x_{name}_v{version}.zip");
+            return 1;
+        }
+
         static int Main(string[] args)
         {
             Console.WriteLine("ar_signtool_cli " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
 
             if (args.Length < 1)
             {
-                Console.WriteLine("USAGE: ar_signtool_cli 'input file or folder' [output zip]");
+                PrintUsage();
                 return 1;
             }
 
             try
             {
-                string input = args[0];
+                string input = null;
+                string output = null;
 
-                // Strip trailing slash
-                if (input.EndsWith(Path.DirectorySeparatorChar.ToString())) {
-                    input = input.Remove(input.Length - 1);
+                for (int n=0; n<args.Length; n++)
+                {
+                    if (input == null)
+                        input = args[n];
+                    else if (output == null)
+                        output = args[n];
+                    else
+                    {
+                        Console.WriteLine("Invalid argument: " + args[n]);
+                        return PrintUsage();
+                    }
                 }
 
+                if (input == null) {
+                    Console.WriteLine("Missing input file argument");
+                    return PrintUsage();
+                }
+
+                // Strip trailing slash
+                if (input.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                {
+                    input = input.Remove(input.Length - 1);
+                }
                 FileInfo inputFi = new FileInfo(input);
 
-                string output = args.Length > 1 ? args[1] : null;
                 if (output == null) {
                     output = System.Environment.CurrentDirectory + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(inputFi.Name) + "_signed.zip";
                 }
                 Console.WriteLine("Input: " + input);
                 Console.WriteLine("Output: " + output);
-                Console.WriteLine("");
 
-                if (inputFi.Attributes == FileAttributes.Directory)
+                if (inputFi.Attributes.HasFlag(FileAttributes.Directory))
                 {
                     Console.WriteLine("Signing folder, please wait...");
                     ar8x_appsignlib.ar8x_appsignlib.SignFolder(input, output);
@@ -49,6 +73,30 @@ namespace ar_signtool_cli
                     Console.WriteLine("Signing zip, please wait...");
                     ar8x_appsignlib.ar8x_appsignlib.SignZipFile(input, output);
                 }
+
+                if (ar8x_appsignlib.ar8x_appsignlib.LastSignedManifest != null)
+                {
+                    // Substitute variables from package manifest
+                    string finalOutput = output;
+                    Console.WriteLine("Package manifest:");
+                    foreach (var pair in ar8x_appsignlib.ar8x_appsignlib.LastSignedManifest)
+                    {
+                        Console.WriteLine($" {pair.Key} = '{pair.Value}'");
+                        finalOutput = finalOutput.Replace($"{{{pair.Key}}}", pair.Value);
+                    }
+                    if (finalOutput != output)
+                    {
+                        if (File.Exists(finalOutput))
+                            File.Delete(finalOutput);
+                        File.Move(output, finalOutput);
+                        Console.WriteLine("Final output: " + finalOutput);
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("WARNING: Missing or invalid properties/manifest file in package");
+                }
+
                 Console.WriteLine("Done");
 
                 return 0;
